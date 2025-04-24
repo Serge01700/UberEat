671f0f0230f0f16b34ea0b2e85ffedb136784b69
Serge01700 <waxqo69@gmail.com>
2025-04-24 09:37:17 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <RestaurantRow />
+     <HomePage />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import RestaurantRow  from './components/RestaurantRow.vue';
+import HomePage from './page/homePage.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('./style/reset.css');
+#app{ 
+  padding: 20px;
 }
+
 </style>
---
+++ src/components/RestaurantRow.vue
@@ -0,0 +1,33 @@
+ <template>
+   <div class="restaurant--row">
+      <h2 class="title">
+        Nos restaurant
+     
+      </h2>
+      <div class="wrapper--card">
+        <RestaurantCard v-for="(card, index) in three_restaurant" 
+        :key="index" :info_restaurant="card"/>
+    </div>
+        
+   </div>
+ </template>
+ 
+ <script setup>
+import RestaurantCard  from './RestaurantCard.vue';
+
+
+defineProps({
+  three_restaurant: Array
+})
+ 
+ </script>
+ 
+ <style lang="scss">
+    .restaurant--row{
+        .wrapper--card{
+            display:flex;
+            justify-content: space-between;
+            align-content: center;
+        }
+    } 
+ </style>
